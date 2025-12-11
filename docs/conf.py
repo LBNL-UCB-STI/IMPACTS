d71d381fcc2b112d62f96a0a49a1431226b52203
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-12-10 20:28:29 -0800

    update project name and adding BEAM_EMFAC-3-withFreight.ipynb

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../src/isrm_wrapper")
+module_dir = os.path.join(__location__, "../src/impacts")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 """
-    Dummy conftest.py for isrm_wrapper.
+    Dummy conftest.py for impacts.
 
     If you don't know what this is for, just leave it empty.
     Read more about conftest.py under:
